d81c748783e8b7d35d2e3f1872bb716e2593783d
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-04-22 00:17:13 +0000

    Remove: Extraneous files Modify: Build files; we want the pretext commands and git commands to run on startup, and this is a test for that.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,17 +3,6 @@
 # Create a new PreTeXt project.
 pretext new -d .
 
-# Build pretext project
-pretext build
-pretext generate
-
-# Add new remote repositories for origin (forked) and upstream (original)
-git remote add upstream https://github.com/twjudson/aata
-# git remote add origin **How do you tell the url of the personal, forked branch when anyone could create one?
-
-# git pull origin main
-# git reset --hard origin/main
-
 # For the Digital System Design book...
 #echo "Installing Digital System Design textbook..."
 #apt install iverilog
