b1e3018704a5123acfc26af39be6accc738a132b
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-04-22 02:29:45 +0000

    Modify: Startup commands for testing

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,11 +1,11 @@
-# Build pretext project
-pretext build
-pretext generate
-
 # Add new remote repositories for origin (forked) and upstream (original)
 git remote add upstream https://github.com/twjudson/aata
-git remote add origin https://github.com/jbrock1999h/aata/tree/main
+git remote add origin https://github.com/jbrock1999h/aata
 
 # Reset to origin main (This is for if the codespace has not caught up to changes made previously)
 git pull origin main
 git reset --hard origin/main
+
+# Build pretext project
+pretext build
+pretext generate
