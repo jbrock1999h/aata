eb27763c83e347de84cc86653d2e45de9b2ce6f4
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-04-22 00:48:27 +0000

    Initial commit

--- .devcontainer/docker-build.sh
+++ .devcontainer/docker-build.sh
@@ -10,14 +10,3 @@ apt install -y --no-install-recommends python3-pip texlive texlive-latex-extra t
 # Upgrade pip and install required Python tools.
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade CodeChat-Server runestone pretext
-
-# Build pretext project
-pretext build
-pretext generate
-
-# Add new remote repositories for origin (forked) and upstream (original)
-git remote add upstream https://github.com/twjudson/aata
-# git remote add origin **How do you tell the url of the personal, forked branch when anyone could create one?
-
-# git pull origin main
-# git reset --hard origin/main
