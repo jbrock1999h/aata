b83d3e7593529a837235f25e406dd59bc7a28ed4
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-04-22 01:02:11 +0000

    Modify: Test

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -4,7 +4,8 @@ pretext generate
 
 # Add new remote repositories for origin (forked) and upstream (original)
 git remote add upstream https://github.com/twjudson/aata
-# git remote add origin **How do you tell the url of the personal, forked branch when anyone could create one?
+git remote add origin https://github.com/jbrock1999h/aata/tree/main
 
-# git pull origin main
-# git reset --hard origin/main
+# Reset to origin main (This is for if the codespace has not caught up to changes made previously)
+git pull origin main
+git reset --hard origin/main
