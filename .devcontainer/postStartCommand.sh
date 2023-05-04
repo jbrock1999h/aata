89fc4635e3d1082a687366e0538ba2156f9b9b00
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-05-03 20:38:04 -0500

    Modify: Already authorized testing

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -33,8 +33,8 @@ echo $GITHUB_TOKEN >> my_token.txt
 #gh auth status
 #echo "auth login number 1 ^ (after refresh)"
 
-#gh repo fork $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
-#echo "Fork created with no issues"
+gh repo fork $GITHUB_SERVER_URL/$GITHUB_REPOSITORY
+echo "Fork created with no issues"
 
 git remote add origin $GITHUB_SERVER_URL/$GITHUB_USER/aata
 echo "Origin added with no issues"
@@ -49,7 +49,7 @@ echo "origin master checked out with no issues"
 # This will start the web server that will allow you to view the textbook after editing it. Usually, you would need to run "pretext build" after altering the textbook, but the --watch tag allows pretext to automatically build when the source has been changed. So the user does not need to know the pretext commands to be able to edit and view their book.
 #pretext view web --watch &
 # The command pretext view web starts a local server which creates a process that does not end until the user manually ends it. This means that any command that comes after pretext view web will not run unless the local server is manually closed.
-#pretext view web &
+pretext view web &
 
 # Running pretext view web from the postStartCommand.sh file does not cause the textbook to automatically open, so we were experimenting on ways to open the port through the command line. It did not work out.
 #xdg-open http://localhost:8000-.preview.app.github.dev
