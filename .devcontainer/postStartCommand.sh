1293a59dea2d3aadfc8e9154489b8e86d256a47f
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-05-05 21:05:07 -0500

    Modify: View Web testing

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -6,7 +6,6 @@
 
 # Add new remote repositories for origin (forked) and upstream (original)
 git remote add upstream https://github.com/twjudson/aata
-echo "Remote upstream add passed with no issues"
 
 rm my_token.txt
 
@@ -39,17 +38,14 @@ echo $GITHUB_TOKEN >> my_token.txt
 #echo "Fork created with no issues"
 
 git remote add origin $GITHUB_SERVER_URL/$GITHUB_USER/aata
-echo "Origin added with no issues"
 
 git fetch origin
-echo "Origin added with no issues"
 
 # The codespace tends to start under the main branch by default, and if the remotes aren't set up correctly you cannot change it to master. This is the line of code that would allow you to automatically set the branch to master if you have origin set:
 git checkout --track origin/master
-echo "origin master checked out with no issues"
 
 # This will start the web server that will allow you to view the textbook after editing it. Usually, you would need to run "pretext build" after altering the textbook, but the --watch tag allows pretext to automatically build when the source has been changed. So the user does not need to know the pretext commands to be able to edit and view their book.
-pretext view web --watch
+sh view_web.sh &
 # The command pretext view web starts a local server which creates a process that does not end until the user manually ends it. This means that any command that comes after pretext view web will not run unless the local server is manually closed.
 #pretext view web &
 
--- view_web.sh
+++ view_web.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-pretext view web --watch
+pretext view web --watch &>/dev/null
+exit 0
